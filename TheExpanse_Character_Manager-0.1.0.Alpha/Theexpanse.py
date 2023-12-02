a78fa64050c38d149260649eaea781f4f0d03100
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2023-12-02 07:14:45 -0600

    formatting and moving most of the code to one file

--- TheExpanse_Character_Manager-0.1.0.Alpha/Theexpanse.py
+++ TheExpanse_Character_Manager-0.1.0.Alpha/Theexpanse.py
@@ -32,10 +32,7 @@ except ImportError:
 
 from CharacterClass import character
 
-from GenericFunctions_MainMenu_CharacterMaker import *
-
-
-
+#WINDOW AND CANVAS
 
 window = tk.Tk()
 window.geometry("500x500")
@@ -44,7 +41,7 @@ window.title("Character Creator by: Luis Alejandro Blake")
 main_canvas = tk.Canvas( window ,bg = "#212121" , height="500", width="500")
 main_canvas.pack()
 
-# Program Variables
+#PROGRAM VARIABLES
 
 currently_making_character = False
 if_origin_already_set = False
@@ -79,7 +76,50 @@ perception.set("")
 strength.set("")
 willpower.set("")
 
-#FUNCTIONS
+#GENERIC FUNCTIONS
+
+def convert_to_die_roll(ability_score):
+    ability_score_table = [[[3],-2], [[4,5],-1],[[6,7,8],0],[[9,10,11],1], [[12,13,14],2], [[15,16,17],3], [[18],4]]
+    
+    for range_and_score in ability_score_table:
+        if(ability_score == range_and_score[1]):
+            print("ability score : " , ability_score , "\n")
+            
+            print("number of that score: " , range_and_score[0][0], "\n")
+            
+            return range_and_score[0][0]
+    print("ERROR: ability score not found\n\n\n")
+   
+def get_characters():
+    characters = []
+    current_character_cards = []
+    Existing_characters_current = []
+    index = 0
+    for image in os.listdir("images\\"):
+        
+        if(str(image) != "noimage.jpg" and str(image) != "photo1.jpg" and str(image) != "Belter.jpg" and str(image) != "TheExpanse-Earther.jpg" and str(image) != "Martian.jpg" and str(image) != "Character_Sheet_1.jpg" and str(image) != "Character_Sheet_2.jpg"):
+           
+            current_character = []
+            current_character.append(str(image))
+            
+            with open("TheExpanseCharacterCreator.txt") as characters_text:
+                characters2 = characters_text.readlines()
+                
+                Character = characters2[index].split(",")
+                
+               
+                temp_character = character(Character[0],Character[1],Character[2],Character[3],Character[4],convert_to_die_roll(int(Character[5])),convert_to_die_roll(int(Character[6])),convert_to_die_roll(int(Character[7])),convert_to_die_roll(int(Character[8])),convert_to_die_roll(int(Character[9])),convert_to_die_roll(int(Character[10])),convert_to_die_roll(int(Character[11])),convert_to_die_roll(int(Character[12])),convert_to_die_roll(int(Character[13])),Character[14],Character[15],Character[16],Character[17])
+
+                Existing_characters_current.append(temp_character)
+             
+                current_character.append(Character[0])
+            characters.append(current_character)
+            
+            index += 1
+    current_character_cards = characters
+    return characters, current_character_cards, Existing_characters_current
+
+#FUNCTIONS FOR MAIN MENU 
 
 def create_character_card(image_path,x,y, parameters,name):
     
@@ -162,6 +202,8 @@ def display_characters(characters, current_character_cards ,Existing_characters_
     Add_characer_button.pack()
     Add_characer_button.place(x=380,y=440)
     window_elements.append(Add_characer_button)    
+   
+#FUNCTIONS FOR CHARACTER DISPLAYING
                 
 def display_character_ClickedOn(ifbackpage,stats):
     global window_elements,displaying_character_background_constant,secondary_buttons_andor_elements
